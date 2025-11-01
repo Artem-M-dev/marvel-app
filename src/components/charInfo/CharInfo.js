7ca8b618ea5dc2549fc1d8989c388fb3fb4ead44
Artem <tema25062010@gmail.com>
2025-11-01 12:50:44 +0300

    Adding PropTypes to the CharList and CharInfo components

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './charInfo.scss';
 import MarvelService from '../../services/MarvelService';
@@ -128,6 +129,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+};
+
+CharInfo.propTypes = {
+    charId: PropTypes.string // Так мы проверям то, что в это свойство будет приходить именно числовой тип
 }
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './charList.scss';
 import MarvelService from '../../services/MarvelService';
@@ -108,7 +109,10 @@ class CharList extends Component {
             </div>
         )
     }
+};
 
+CharList.propTypes = {
+    onCharSelected: PropTypes.bool
 }
 
 export default CharList;
